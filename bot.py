c1daa88afca2bf7f2e28208d5b530e6f78d2672a
knox <111348305+aceknox@users.noreply.github.com>
2023-02-19 08:29:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 
 import os
 from config import Config
-from pyrogram import Client as Clinton
+from pyrogram import Client
 
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton("@urluploaderadvancedbot",
+    Warrior = Client("@urluploaderadvancedbot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
